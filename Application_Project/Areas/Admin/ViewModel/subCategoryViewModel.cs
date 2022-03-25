d542d9d973b24b6bc20766ab2507bd6de356321e
adii1810 <adityaneve63@gmail.com>
2022-03-25 18:03:39 +0530

    init created

---
+++ Application_Project/Areas/Admin/ViewModel/subCategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application_Project.Areas.Admin.ViewModel
+{
+    public class subCategoryViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
